e5005e37c7d8349c3a786b7f851f8555ed23f1a6
Zul-abdin <zulabdin4463@gmail.com>
2019-10-14 16:07:15 -0400

    Initial Commit

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"chess","c":"Chess","l":"Chess()","url":"%3Cinit%3E()"},{"p":"chess","c":"Chess","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ src/chess/Chess.java
@@ -0,0 +1,13 @@
+package chess;
+
+/**
+ * @author Zain Ul-Abdin
+ * @author Kirtan Patel
+ */
+public class Chess {
+
+    public static void main(String[] args) {
+
+    }
+
+}
